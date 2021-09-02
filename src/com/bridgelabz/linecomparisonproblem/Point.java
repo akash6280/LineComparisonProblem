9e23c890fd7a3c792f30ae33b9c29f945e4fca7d
Akash Singh <akashsingh6280@gmail.com>
2021-09-02 22:59:17 +0530

    ADDED: Point class which has coordinates of point

---
+++ src/com/bridgelabz/linecomparisonproblem/Point.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.linecomparisonproblem;
+
+public class Point {
+		private double xCoordinate;
+		private double yCoordinate;
+		
+		public Point(double xCoordinate, double yCoordinate) {
+			this.xCoordinate=xCoordinate;
+			this.yCoordinate=yCoordinate;
+		}
+		public double getxCoordinate() {
+			return xCoordinate;
+		}
+		public void setxCoordinate(double xCoordinate) {
+			this.xCoordinate = xCoordinate;
+		}
+		public double getyCoordinate() {
+			return yCoordinate;
+		}
+		public void setyCoordinate(double yCoordinate) {
+			this.yCoordinate = yCoordinate;
+		}
+		
+		
+}
