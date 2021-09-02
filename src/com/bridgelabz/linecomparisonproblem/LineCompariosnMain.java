538b41c171174b7dcddbccd20f8a5d3f1db6b228
Akash Singh <akashsingh6280@gmail.com>
2021-09-02 22:32:42 +0530

    ADDED: Checking equality of two lines

--- src/com/bridgelabz/linecomparisonproblem/LineCompariosnMain.java
+++ src/com/bridgelabz/linecomparisonproblem/LineCompariosnMain.java
@@ -5,16 +5,27 @@ public class LineCompariosnMain {
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		
-		System.out.println("Enter coordinates of line");
+		System.out.println("Enter coordinates of line 1 as (x1,y1) and (x2,y2)");
 		double xStartCoordinate=scanner.nextDouble();
 		double yStartCoordinate=scanner.nextDouble();
 		double xEndCoordinate=scanner.nextDouble();
 		double yEndCoordinate=scanner.nextDouble();
 		
-		double lengthOfLine=Math.sqrt((xStartCoordinate-xEndCoordinate)*(xStartCoordinate-xEndCoordinate)+(yStartCoordinate-yEndCoordinate)*(yStartCoordinate-yEndCoordinate));
+		Double lengthOfLine1=Math.sqrt((xStartCoordinate-xEndCoordinate)*(xStartCoordinate-xEndCoordinate)+(yStartCoordinate-yEndCoordinate)*(yStartCoordinate-yEndCoordinate));
 		
-		System.out.printf("Length of line is %.2f",lengthOfLine);
 		
+		System.out.println("Enter coordinates of line 2 as x1,y1) and (x2,y2)");
+		xStartCoordinate=scanner.nextDouble();
+		yStartCoordinate=scanner.nextDouble();
+		xEndCoordinate=scanner.nextDouble();
+		yEndCoordinate=scanner.nextDouble();
+		
+		Double lengthOfLine2=Math.sqrt((xStartCoordinate-xEndCoordinate)*(xStartCoordinate-xEndCoordinate)+(yStartCoordinate-yEndCoordinate)*(yStartCoordinate-yEndCoordinate));
+		
+		if(lengthOfLine1.equals(lengthOfLine2))
+			System.out.println("Lines are equal");
+		else
+			System.out.println("Lines are unequal");
 		scanner.close();
 		
 	}
