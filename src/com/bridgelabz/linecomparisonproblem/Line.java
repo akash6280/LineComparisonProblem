749b5fddd1c4494917cd87acc4b33c7f5c7b28a4
Akash Singh <akashsingh6280@gmail.com>
2021-09-02 23:12:05 +0530

    ADDED: Line class which has start and end point of line

---
+++ src/com/bridgelabz/linecomparisonproblem/Line.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linecomparisonproblem;
+
+public class Line {
+	
+	 Point startPoint;
+	 Point endPoint;
+	
+	public Line(Point startPoint, Point endPoint) {
+		this.startPoint = startPoint;
+		this.endPoint = endPoint;
+	}
+}
