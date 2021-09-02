325c4e92b8b3fb446acb3825b29603c86316ed36
Akash Singh <akashsingh6280@gmail.com>
2021-09-02 23:28:04 +0530

    ADDED: Interface to abstract length comparator methods

---
+++ src/com/bridgelabz/linecomparisonproblem/LengthComparatorIF.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.linecomparisonproblem;
+
+public interface LengthComparatorIF {
+	public void equals(Line firstLine, Line secondLine);
+	public void compareTo(Line firstLine, Line secondLine);
+}
